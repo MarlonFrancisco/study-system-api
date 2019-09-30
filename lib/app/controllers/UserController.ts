2266d21c58b5e03d4c04b3133ded678d219aee99
Marlon Francisco <marlon@biggy.com.br>
2019-09-29 21:26:58 -0300

    add route user

--- lib/app/controllers/UserController.ts
+++ lib/app/controllers/UserController.ts
@@ -1,8 +1,26 @@
 import { Request, Response, Router } from "express";
+import { get } from "mongoose";
+import User from "../models/User";
+import { IRequest } from "../../typings/express";
 const router = Router();
 
 class UserController {
     public router = router;
+
+    @get("/")
+    public async get(req: IRequest, res: Response) {
+        try {
+            const user = await User.findById(req.userId);
+
+            if (!user) {
+                return res.status(400).send("user not found");
+            }
+
+            return res.send(user);
+        } catch (err) {
+            return res.status(400).send(err);
+        }
+    }
 }
 
 export default new UserController().router;
