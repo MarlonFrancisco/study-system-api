86e31e70e7aca65ab646ef1463364ab4dba7e30e
Marlon Francisco <marlon@biggy.com.br>
2019-11-25 22:27:07 -0300

    .

--- lib/app/controllers/UserController.ts
+++ lib/app/controllers/UserController.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
 import { resolve } from "path";
 import app from "../../app";
-import { Get, Post } from "../../helpers/decorators";
+import { Get, Post, Put } from "../../helpers/decorators";
 import { IRequest } from "../../interfaces/express";
 import User from "../models/User";
+import { IUser } from "../../interfaces/UserModel";
 
 const router = Router();
 
@@ -13,7 +14,10 @@ class UserController {
     @Get("/", router)
     public async get(req: IRequest, res: Response) {
         try {
-            const user = await User.findById(req.userId).populate(["lessons", "subjects"]);
+            const user = await User.findById(req.userId).populate([
+                "lessons",
+                "subjects",
+            ]);
 
             if (!user) {
                 return res.status(400).send("not found user");
@@ -35,14 +39,94 @@ class UserController {
             if (!user) {
                 return res.status(400).send("Not found user");
             }
-            const templatePath = resolve("lib", "app", "views", "mail", "contactus.html");
-            const status = await app.mailer.sender(templatePath, [
-                {name: "name", value: user.name },
-                {name: "email", value: user.email },
-                {name: "message", value: message},
-            ], subject, process.env.EMAIL);
+            const templatePath = resolve(
+                "lib",
+                "app",
+                "views",
+                "mail",
+                "contactus.html",
+            );
+            const status = await app.mailer.sender(
+                templatePath,
+                [
+                    { name: "name", value: user.name },
+                    { name: "email", value: user.email },
+                    { name: "message", value: message },
+                ],
+                subject,
+                process.env.EMAIL,
+            );
 
             return res.send(status);
+        } catch (err) {
+            return res.status(400).send(err);
+        }
+    }
+
+    @Put("/infos", router)
+    public async infos(req: IRequest, res: Response) {
+        try {
+            const { name, email } = req.body;
+            let user: IUser;
+            if (name && email) {
+                user = await User.findByIdAndUpdate(
+                    req.userId,
+                    {
+                        $set: {
+                            name,
+                            email,
+                        },
+                    },
+                    { new: true },
+                );
+            }
+
+            if (name && !email) {
+                user = await User.findByIdAndUpdate(
+                    req.userId,
+                    {
+                        $set: {
+                            name,
+                        },
+                    },
+                    { new: true },
+                );
+            }
+
+            if (!name && email) {
+                user = await User.findByIdAndUpdate(
+                    req.userId,
+                    {
+                        $set: {
+                            email,
+                        },
+                    },
+                    { new: true },
+                );
+            }
+
+            return res.send(user);
+        } catch (err) {
+            return res.status(400).send(err);
+        }
+    }
+
+    @Put("/password", router)
+    public async password(req: IRequest, res: Response) {
+        try {
+            const { password } = req.body;
+
+            const user = await User.findByIdAndUpdate(req.userId, {
+                $set: {
+                    password,
+                },
+            });
+
+            if (!user) {
+                return res.status(400).send("Failed change password");
+            }
+
+            return res.send(user);
         } catch (err) {
             return res.status(400).send(err);
         }
